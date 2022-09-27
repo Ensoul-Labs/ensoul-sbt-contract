239f4345c4cfcdc9de21f13fd09d651c529e7ba9
kasodesyn <bokang.qian@desyn.io>
2022-09-27 22:05:43 +0800

    完成库分割，将库移到本地方便管理

--- contracts/Example.sol
+++ contracts/Example.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.12;
 
-import "./interfaces/IExample.sol";
+import "./src/interfaces/IExample.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
