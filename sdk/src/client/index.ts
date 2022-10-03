48efc6072f86c6a0538901b9ac835c1771bdced1
33357 <1043898730@qq.com>
2022-10-03 10:01:57 +0100

    编写 sdk

--- sdk/src/client/index.ts
+++ sdk/src/client/index.ts
@@ -1,7 +1,5 @@
-export * from './ExampleUpgradeableClient';
-export * from './ExampleClient';
-export * from './ERC20Client';
+export * from './EnsoulClient';
+export * from './EnsoulFactoryClient';
 
-export * from './EtherExampleClient';
-export * from './EtherExampleUpgradeableClient';
-export * from './EtherERC20Client';
+export * from './EtherEnsoulClient';
+export * from './EtherEnsoulFactoryClient';
